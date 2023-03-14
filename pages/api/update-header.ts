fde72c2035048c26cdd49cda7cea23e32e5ab916
rolldavid <rolldev@proton.me>
2023-03-13 17:36:53 -0700

    header types

--- pages/api/update-header.ts
+++ pages/api/update-header.ts
@@ -1,21 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
-/* import { getServerSession } from "next-auth/next"
-import { authOptions } from "./auth/[...nextauth]"; */
+
 import prisma from "@/lib/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
    
         const { url, boardId } = req.body;
         
-        const board = await prisma.board.update({
-            where: {
-                id: boardId
-            },
-            data: {
-                headerURL: url,
-                customHeader: true
-            }
-          })
 
-        res.status(201).json({status: "ok"})
+        if (typeof url === "string") {
+            const board = await prisma.board.update({
+                where: {
+                    id: boardId
+                },
+                data: {
+                    headerURL: url,
+                    customHeader: true
+                }
+              })
+    
+            res.status(201).json({status: "ok"})
+        }
+        
 }
