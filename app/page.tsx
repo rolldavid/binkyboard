2a778ff11a5c21cd19af67db97b22ccebecf45b4
rolldavid <rolldev@proton.me>
2023-03-18 13:52:01 -0700

    meta without og

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,7 @@ export default function Page() {
   return <Landing />
 }
 
-
 export const metadata: Metadata = {
   title: "binkyboard",
-  description: "A simple place to celebrate life's biggest moments",
-  openGraph: {
-    title: "binkyboard",
-    description: "A simple place to celebrate life's biggest moments",
-    url: `https://binkyboard.com`,
-    images: [
-      {
-      url: `https://d3h42dhdxazsqn.cloudfront.net/yPwbiLyA-e_APVJ1vI6MS.png`,
-      width: 1200,
-      height: 675,
-      alt: "binkyboard"
-      }
-    ],
-    type: "website",
-    locale: "en-US"
-  },
+  description: "A simple place to celebrate life's biggest moments"
 }
