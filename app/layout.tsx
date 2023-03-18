dd24812205c62e4646dc5648e41fc4f7a26fc448
rolldavid <rolldev@proton.me>
2023-03-18 12:03:02 -0700

    og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,13 +45,13 @@ export const metadata = {
     title: "binkyboard",
     description: "A simple place to celebrate life's biggest moments",
     url: `https://binkyboard.com/`,
-    images: [
+    image: 
       {
       url: `https://d3h42dhdxazsqn.cloudfront.net/yPwbiLyA-e_APVJ1vI6MS.png`,
       width: 1200,
       height: 675
       }
-    ],
+    ,
     type: "website",
     locale: "en-US"
   }
