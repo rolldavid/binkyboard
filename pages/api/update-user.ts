2952538cbfbd06f3a0e19b9ac63a4e6583e9ea6d
rolldavid <rolldev@proton.me>
2023-03-25 16:52:39 -0700

    update user

--- pages/api/update-user.ts
+++ pages/api/update-user.ts
@@ -3,14 +3,13 @@ import { getSession } from '@auth0/nextjs-auth0';
 import prisma from "@/lib/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log("update...........")
+
     try {
         const session = await getSession(req, res)
+        console.log(session, "session))))))))))))))))")
         if (!session) {
             return res.status(201).json({session: false, userId: undefined})
         }
-
-        console.log(session.user, "=======================")
 
         if (session?.user?.email) {
             const upsertUser = await prisma.user.upsert({
