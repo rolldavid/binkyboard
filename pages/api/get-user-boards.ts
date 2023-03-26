e73a9c8c997bcd130968759054854af2cd56193a
rolldavid <rolldev@proton.me>
2023-03-26 10:46:43 -0700

    get boards

--- pages/api/get-user-boards.ts
+++ pages/api/get-user-boards.ts
@@ -20,10 +20,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 }
             })
           
-            if (userBoards && userBoards.role) {
-                res.status(201).json({boards: userBoards?.boards, role: userBoards.role})
+            if (userBoards) {
+                res.status(201).json({boards: userBoards?.boards, role: userBoards?.role})
+                return
             }
-            res.status(201).json({boards: userBoards?.boards, role: "USER"})
             
         } else {
             throw new Error("Did not manage to connect")
