c6117418193d28c6e6b7afc4480b1841a2a4060c
rolldavid <rolldev@proton.me>
2023-03-26 17:30:23 -0700

    remove invite

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,7 @@ import styles from "@/styles/Home.module.css"
 import { updateUser } from "@/lib/db-utils";
 
 export default function Page() {
-  const [invite, setInvite] = useState("")
   const {isLoading, user, error} = useUser()
-
-  useEffect(() => {
-    const inviteLink = localStorage.getItem("invite")
-    if (inviteLink) {
-      setInvite(inviteLink)
-    }
-  }, [])
 
   
   if (isLoading) {
@@ -34,7 +26,7 @@ export default function Page() {
   }
 
   if (user) {
-    return <Landing invite={invite}/>
+    return <Landing />
   }
 
   return null;
