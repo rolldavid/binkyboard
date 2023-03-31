059e7cb7110fcc6ca484b42ffc72324d0cc60c27
rolldavid <rolldev@proton.me>
2023-03-31 09:39:50 -0700

    add specific metadata

---
+++ app/board/[id]/layout.tsx
@@ -0,0 +1,33 @@
+
+export default async function Layout({ children }: {
+    children: React.ReactNode;
+  }) {
+ 
+    return (
+        <>{children}</>
+    );
+  }
+
+
+  export async function generateMetadata({params: {id}}: {params: { id: string }}) {
+
+    return {
+        title: "binkyboard",
+        description: "A simple place to celebrate life's biggest moments",
+        openGraph: {
+            title: "Join David on binkyboard!",
+            description: "A simple place to celebrate life's biggest moments",
+            url: `https://binkyboard.com`,
+            images: [
+            {
+            url: `https://d3h42dhdxazsqn.cloudfront.net/yPwbiLyA-e_APVJ1vI6MS.png`,
+            width: 1200,
+            height: 675,
+            alt: "binkyboard"
+            }
+            ],
+            type: "website",
+            locale: "en-US"
+        },
+    };
+  }
