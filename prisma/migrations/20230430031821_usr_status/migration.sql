40cac31265101f135c19341d3127452e1d325a5e
rolldavid <rolldev@proton.me>
2023-04-29 20:19:35 -0700

    revert and add string user status

---
+++ prisma/migrations/20230430031821_usr_status/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - The `status` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "status",
+ADD COLUMN     "status" TEXT DEFAULT 'ACTIVE';
+
+-- DropEnum
+DROP TYPE "UserStatus";
