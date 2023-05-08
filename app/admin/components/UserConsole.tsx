64d116f0f903890569e5b5bf91cf86c6a457e09b
rolldavid <rolldev@proton.me>
2023-05-08 08:42:31 -0700

    admin auth

--- app/admin/components/UserConsole.tsx
+++ app/admin/components/UserConsole.tsx
@@ -1,13 +1,37 @@
+"use client"
 
+import { useQuery } from "@tanstack/react-query"
+import { useRouter } from "next/navigation"
+import { getUserId } from "@/lib/db-utils"
 import Search from "./Search"
+import Spinner from "@/lib/Spinner"
 import styles from "./UserConsole.module.css"
 
 export default function UserManagement () {
-    return (
-        <div className={styles.container}>
-            <div className={styles.searchContainer}>
-                <Search />
+
+    const router = useRouter()
+
+    const {data, status} = useQuery(["userId"], () => {
+        return getUserId()
+    })
+
+    if (status === "loading") {
+        return <Spinner />
+    }
+
+    if (status === "success" && data.role === "ADMIN") {
+        return (
+            <div className={styles.container}>
+                <div className={styles.searchContainer}>
+                    <Search />
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
+    if (status === "success" && data) {
+        router.push("/")
+    }
+
+    return null;
 }
