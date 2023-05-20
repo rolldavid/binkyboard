55a94e3303e98ce94e77349e72c56d2a5d037eec
rolldavid <rolldev@proton.me>
2023-05-19 20:23:47 -0700

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default async function RootLayout({
             </footer>
             </QueryProvider>
           </UserProvider>
+          <Analytics />
           
       </body>
     </html>
